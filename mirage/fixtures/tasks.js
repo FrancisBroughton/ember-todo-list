3621a175424ebbf63ee471de4535fea3b9c2fb9e
FrancisBroughton <iamfrancisbroughton@gmail.com>
2018-03-02 17:10:11 +0000

    starting todo lost enabled

---
+++ mirage/fixtures/tasks.js
@@ -0,0 +1,18 @@
+export default [
+  { id: '1',
+  description: 'Pack holiday bag',
+  isDone: false
+}, {
+  id: '2',
+  description: 'Book taxi',
+  isDone: false
+}, {
+  id: '3',
+  description: 'get currency',
+  isDone: false
+}, {
+  id: '4',
+  description: 'drop cats off',
+  isDone: false
+}
+];
